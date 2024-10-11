5078d0111cae2ef0523cf1d51a13c83a29e22b66
Aleš Zima <zima.ales@seznam.cz>
2024-10-11 11:27:36 +0200

    Added zpracuj_odpoved function

--- lokace.py
+++ lokace.py
@@ -1,17 +1,26 @@
 class Lokace:
     jmeno: str
+    uvitaci_text: str
     hranice: list
 
     # Volá se pří vytváření nové instance
-    def __init__(self, jmeno):
+    def __init__(self, jmeno, uvitaci_text=None):
         self.jmeno = jmeno
+        self.uvitaci_text = (
+            uvitaci_text
+            if uvitaci_text is not None
+            else f"Nachážíš se v lokaci {jmeno}. Kam půjdeš?"
+        )
 
     # Definuje výstup funkce print(Lokace)
     def __str__(self):
         return self.jmeno
 
 
-hobitin = Lokace("Hobitín")
+hobitin = Lokace(
+    "Hobitín",
+    "Stojíš na začátku své cesty ve vesničce Hobitín, obklopený zelenými kopci, kde se nízké, okrouhlé domky hobitů s trávou porostlými střechami schovávají mezi rozkvetlými zahradami. Slunce svítí na barevné záhony květin a kolem tebe je slyšet zpěv ptáků a vzdálený smích místních obyvatel. Vzduch voní čerstvým chlebem a pípou z blízké hospody, kde začíná každé dobré hobití dobrodružství. Otevřené cesty se vinou mezi zahradami a lákají tě, aby ses vydal dál. Ale kam tě nohy zavedou?",
+)
 brandyvina = Lokace("Most přes Brandyvínu")
 belobrazda = Lokace("Bělobrázda")
 hurka = Lokace("Hůrka")
--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import lokace
 
 
+def zpracuj_odpoved(pocet_moznosti: int):
+    spravny_input = False
+
+    while spravny_input is not True:
+        vyber = input("> ")
+        try:
+            vyber = int(vyber)
+        except Exception:
+            pass
+
+        if type(vyber) is int and vyber >= 0 and vyber < pocet_moznosti:
+            return vyber
+        else:
+            print("Zkus to znovu.")
+            print("\n")
+
+
 def zmen_lokaci(nova_lokace: lokace.Lokace):
     akt_lokace = nova_lokace
 
-    print(f"Nachážíš se v lokaci {akt_lokace}. Kam půjdeš?")
+    print("\n")
+    print(akt_lokace.uvitaci_text)
 
     for index, sousedni_lokace in enumerate(akt_lokace.hranice):
         print(f"[{index}] {sousedni_lokace}")
 
-    vyber = input("> ")
-
-    try:
-        vyber = int(vyber)
-    except Exception:
-        pass
-
-    if type(vyber) is int and vyber >= 0 and vyber < len(akt_lokace.hranice):
-        zmen_lokaci(akt_lokace.hranice[vyber])
-    else:
-        print("Zkus to znovu.")
-        print("___")
-        zmen_lokaci(akt_lokace)
+    vyber = zpracuj_odpoved(len(akt_lokace.hranice))
+    zmen_lokaci(akt_lokace.hranice[vyber])
 
 
+# Start hry
 zmen_lokaci(lokace.hobitin)
